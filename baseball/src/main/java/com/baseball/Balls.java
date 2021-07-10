99c75b2c9e68470e0ae38ee6d652a61ba4da1f35
wehwan <we_hwan@naver.com>
2021-07-10 21:51:43 +0900

    feat: 무작위로 3가지 공을 만드는 기능 추가

--- baseball/src/main/java/com/baseball/Balls.java
+++ baseball/src/main/java/com/baseball/Balls.java
@@ -1,12 +1,37 @@
 package com.baseball;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Balls {
 
+    private static final int RANDOM_BALL_SIZE = 3;
+    private static final int RANDOM_BALL_NUMBER_MAX_BOUND = 10;
+
     private final List<Ball> balls;
+
+    public Balls() {
+        this(createRandomBalls(RANDOM_BALL_SIZE));
+    }
+
+    private static List<Ball> createRandomBalls(int size) {
+        List<Ball> randomBalls = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            randomBalls.add(new Ball(createRandomNumber()));
+        }
+        return null;
+    }
+
+    private static int createRandomNumber() {
+        int number = 0;
+        while(number <= 0 || number >= RANDOM_BALL_NUMBER_MAX_BOUND) {
+            number = new Random().nextInt(RANDOM_BALL_NUMBER_MAX_BOUND);
+        }
+        return number;
+    }
 
     public Balls(String balls) {
         this(convertBalls(balls));
