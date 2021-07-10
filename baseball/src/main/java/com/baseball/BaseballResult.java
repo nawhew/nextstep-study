f9899d9bdccbb4eec2c4d6b6e53249ca505f3772
wehwan <we_hwan@naver.com>
2021-07-10 19:50:19 +0900

    refactor: 결과 출력 메소드 이동

--- baseball/src/main/java/com/baseball/BaseballResult.java
+++ baseball/src/main/java/com/baseball/BaseballResult.java
@@ -1,5 +1,7 @@
 package com.baseball;
 
+import java.util.Map;
+
 public enum  BaseballResult {
     STRIKE("스트라이크")
     , BALL("볼")
@@ -59,5 +61,56 @@ public enum  BaseballResult {
      */
     private static boolean isBall(Ball userBall, int userBallIndex, Ball computerBall, int computerBallIndex) {
         return userBall.equals(computerBall) && userBallIndex != computerBallIndex;
+    }
+
+    /**
+     * 축적된 결과를 보기 좋게 문자열로 변환하여 반환합니다.
+     * 포맷 : N 스트라이크 M 볼
+     * 포맷 2 (스트라이크와 볼이 없고 낫싱만 3인 경우) : 낫싱
+     * @return
+     */
+    public static String getPrettyPrintResultString(Map<BaseballResult, Integer> results) {
+        String result = "";
+
+        if (isPrintNothing(results)) {
+            return BaseballResult.NOTHING.getDisplayName();
+        }
+        result = addPrintStrike(result, results);
+        result = addPrintBall(result, results);
+        return result;
+    }
+
+    /**
+     * 볼이 있는 경우 볼을 출력하기 위한 문자열을 가져 옵니다
+     * @param result
+     * @return
+     */
+    private static String addPrintBall(String result, Map<BaseballResult, Integer> results) {
+        Integer ballCount = results.get(BaseballResult.BALL);
+        if(ballCount != null && ballCount != 0) {
+            if(result.length() != 0) {
+                result = result + " ";
+            }
+            result = result + ballCount + " " + BaseballResult.BALL.getDisplayName();
+        }
+        return result;
+    }
+
+    /**
+     * 스트라이크가 있는 경우 스트라이크를 출력하기 위한 문자열을 가져옵니다.
+     * @param result
+     * @return
+     */
+    private static String addPrintStrike(String result, Map<BaseballResult, Integer> results) {
+        Integer strikeCount = results.get(BaseballResult.STRIKE);
+        if(strikeCount != null && strikeCount != 0) {
+            result = strikeCount + " " + BaseballResult.STRIKE.getDisplayName();
+        }
+        return result;
+    }
+
+    private static boolean isPrintNothing(Map<BaseballResult, Integer> results) {
+        Integer nothingCount = results.get(BaseballResult.NOTHING);
+        return nothingCount != null && nothingCount == 3;
     }
 }
--- baseball/src/main/java/com/baseball/PlayBaseball.java
+++ baseball/src/main/java/com/baseball/PlayBaseball.java
@@ -22,57 +22,6 @@ public class PlayBaseball {
             BaseballResult baseballResult = BaseballResult.calculateResult(userBall, i, computerBalls);
             results.put(baseballResult, results.getOrDefault(baseballResult, 0) + 1);
         }
-        return this.getPrettyPrintResultString(results);
-    }
-
-    /**
-     * 축적된 결과를 보기 좋게 문자열로 변환하여 반환합니다.
-     * 포맷 : N 스트라이크 M 볼
-     * 포맷 2 (스트라이크와 볼이 없고 낫싱만 3인 경우) : 낫싱
-     * @return
-     */
-    private String getPrettyPrintResultString(Map<BaseballResult, Integer> results) {
-        String result = "";
-
-        if (this.isPrintNothing(results)) {
-            return BaseballResult.NOTHING.getDisplayName();
-        }
-        result = this.addPrintStrike(result, results);
-        result = this.addPrintBall(result, results);
-        return result;
-    }
-
-    /**
-     * 볼이 있는 경우 볼을 출력하기 위한 문자열을 가져 옵니다
-     * @param result
-     * @return
-     */
-    private String addPrintBall(String result, Map<BaseballResult, Integer> results) {
-        Integer ballCount = results.get(BaseballResult.BALL);
-        if(ballCount != null && ballCount != 0) {
-            if(result.length() != 0) {
-                result = result + " ";
-            }
-            result = result + ballCount + " " + BaseballResult.BALL.getDisplayName();
-        }
-        return result;
-    }
-
-    /**
-     * 스트라이크가 있는 경우 스트라이크를 출력하기 위한 문자열을 가져옵니다.
-     * @param result
-     * @return
-     */
-    private String addPrintStrike(String result, Map<BaseballResult, Integer> results) {
-        Integer strikeCount = results.get(BaseballResult.STRIKE);
-        if(strikeCount != null && strikeCount != 0) {
-            result = strikeCount + " " + BaseballResult.STRIKE.getDisplayName();
-        }
-        return result;
-    }
-
-    private boolean isPrintNothing(Map<BaseballResult, Integer> results) {
-        Integer nothingCount = results.get(BaseballResult.NOTHING);
-        return nothingCount != null && nothingCount == 3;
+        return BaseballResult.getPrettyPrintResultString(results);
     }
 }
